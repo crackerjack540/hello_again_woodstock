4da3f486242b0f06d2bef65c298412e029d2da81
crackerjack540 <awheat1989@gmail.com>
2016-11-14 13:40:43 -0500

    updated deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,28 +1,21 @@
-# config valid only for Capistrano 3.1
-lock '3.1.0'
-
 set :application, 'hello_again_woodstock'
 set :repo_url, 'https://github.com/crackerjack540/hello_again_woodstock.git'
+set :rvm_type, :user
 
+set :deploy_to, '/home/deploy/hello_again_woodstock'
+
+set :linked_files, %w{config/database.yml}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :deploy do
 
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
+      execute :touch, release_path.join('tmp/restart.txt')
     end
   end
 
-  after :publishing, :restart
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-       execute :touch, release_path.join('tmp/restart.txt')
-    end
-  end
-  
   after :publishing, 'deploy:restart'
   after :finishing, 'deploy:cleanup'
 end
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,3 @@
 set :stage, :production
 
-server '138.197.16.198', user: 'deploy', roles: %w{web app}
+server '138.197.16.198', user: 'deploy', roles: %w{web app db}
